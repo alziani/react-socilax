2900a77e8de20c2872c21e0b4df507b9bc9451ed
ziani abderrahim <zianiabderrahim@gmail.com>
2023-07-14 01:25:24 +0100

    finishing values section

--- src/components/value/Value.jsx
+++ src/components/value/Value.jsx
@@ -1,38 +1,60 @@
 import React from "react";
 import valueImage from "../../images/valueImage.png";
 import "./value.css";
-import ValueAccordion from "./ValueAccordion";
-import valueData from "./valueData.json";
+import valueData from "./valueData";
+
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+  AccordionItemState,
+} from "react-accessible-accordion";
+import "react-accessible-accordion/dist/fancy-example.css";
+import { MdOutlineArrowDropDown } from "react-icons/md";
 
 function Value() {
   return (
     <section className="value-wrapper">
-      <div className="paddings innerWidth  value-container ">
+      <div className="paddings innerWidth flexCenter value-container ">
         <div className=" value-left">
           <div className="image-container">
             <img src={valueImage} />
           </div>
         </div>
-        <div className="value-right">
-          <div className=" flexColStart r-head">
-            <span className="orangeText">Our Value</span>
-            <span className="primaryText">Value We Give to You</span>
-            <span className="secondaryText">
-              We always ready to help by providijng the best services for you.{" "}
-              <br />
-              We beleive a good blace to live can make your life better
-            </span>
-          </div>
-          <div>
-            {valueData.map((data, indx) => (
-              <ValueAccordion
-                key={indx}
-                title={data.title}
-                text={data.description}
-                eventKey={indx}
-              />
-            ))}
-          </div>
+        <div className="flexColStart value-right">
+          <span className="orangeText">Our Value</span>
+          <span className="primaryText">Value We Give to You</span>
+          <span className="secondaryText">
+            We always ready to help by providijng the best services for you.{" "}
+            <br />
+            We beleive a good blace to live can make your life better
+          </span>
+          <Accordion
+            className="accordion"
+            allowMultipleExpanded={false}
+            preExpanded={[0]}
+          >
+            {valueData.map((item) => {
+              return (
+                <AccordionItem key={item.id} className="accordionItem">
+                  <AccordionItemHeading>
+                    <AccordionItemButton className="accordionButton">
+                      <div className="flexCenter icon">{item.icon}</div>
+                      <span className="primaryText">{item.heading}</span>
+                      <div className="flexCenter icon">
+                        <MdOutlineArrowDropDown size={28} />
+                      </div>
+                    </AccordionItemButton>
+                  </AccordionItemHeading>
+                  <AccordionItemPanel>
+                    <p className="secondaryText">{item.detail}</p>
+                  </AccordionItemPanel>
+                </AccordionItem>
+              );
+            })}
+          </Accordion>
         </div>
       </div>
     </section>
